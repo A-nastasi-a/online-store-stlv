0055e2c2f147b12f4c928cdfc272d620ed7f5a51
Anastasia <evsihneyeva@gmail.com>
2023-03-23 17:48:09 +0400

    feat: add models

---
+++ server/models/models.js
@@ -0,0 +1,46 @@
+const sequelize = require('../db.js');
+const {DataTypes} = require('sequelize');
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'}
+});
+
+const Tray = sequelize.define('tray', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+const TrayDish = sequelize.define('tray_dish', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+const Dish = sequelize.define('dish', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    rating: {type: DataTypes.FLOAT, allowNull: false},
+    img: {ttpe: DataTypes.STRING, allowNull: false},
+});
+
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+});
+
+const CookingMethod = sequelize.define('cooking_method', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+});
+
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.FLOAT, allowNull: false}
+});
+
+const DishInfo = sequelize.define('dish_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, unique: true, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false}
+});
